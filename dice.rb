e1ef39a7aa5d004f82a76abf72a675ccd9c17b82
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-09 02:43:12 +0000

    mettre a jour

--- dice.rb
+++ dice.rb
@@ -2,7 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "Hello World"
+  "<h1>Dice Roll</h1>
+   <ul>
+    <li><a href="/dice/2/6"><h2>Roll two 6-sided dice</h2></a></li>
+    <li><a href="/dice/2/10"><h2>Roll two 10-sided dice</h2></a></li>
+    <li><a href="/dice/1/20"><h2>Roll one 20-sided dice</h2></a></li>
+    <li><a href="/dice/5/4"><h2>Roll five 4-sided dice</h2></a></li>
+   </ul>"
 end
 
 get("/zebra") do
@@ -21,5 +27,39 @@ get("/dice/2/6") do
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
   "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+	
+  outcome = "You rolled a #{first_die}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
    <p>#{outcome}</p>"
 end
